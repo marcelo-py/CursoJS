593d7857f1c0c065175fea0f1502aff5c2d34780
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-05-30 22:33:26 -0300

    prática de condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 50
+console.log(`A velocidade do carro é ${vel}Km/h`)
+if (vel >= 60) {
+    console.log('Vc ultrapassou o limite, toma essa multa')
+}
+console.log('Diriga sempre com cuidado!')
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'brasil'.toLowerCase()
+
+console.log(`Seu país é: ${pais}`)
+
+if (pais == 'brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é estrangeiro!')
+}

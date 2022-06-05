c052bd6d937b2832a2d49ae81d4cfcbebec09ef0
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-05 15:48:21 -0300

    testes não muito relevantes

---
+++ aula12ex/desafio01/testes/script.js
@@ -0,0 +1,7 @@
+var div = document.getElementsByTagName('div')[0]
+var div2 = document.createElement('div')
+var img = document.createElement('img')
+div2.setAttribute('id', 'div2')
+img.setAttribute('src', 'adultamulher.jpg')
+img.style.maxWidth = '400px'
+div.append(div2, img)

71af01ded12fc99a4a31abef48a0ee4c80e65604
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-05 17:05:40 -0300

    testando whiles e do/while

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+
+var cont = 0
+do {
+    console.log('Tudo bem?')
+    cont ++
+} while (cont < 7)
+
+
+/*var cont = 0
+while (cont <6) {
+    console.log('Tudo bem?')
+    cont++
+    if (cont == 4) {
+        console.log('Tirado quando esteve no 4')
+    }
+}*/
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/

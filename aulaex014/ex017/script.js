03f8d1f85123026c3b7c9284df7bcd20b4c94ef0
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-05 23:30:35 -0300

    ex017 finished (Segura a mais nova fera do JS)

--- aulaex014/ex017/script.js
+++ aulaex014/ex017/script.js
@@ -6,6 +6,7 @@ function calcular() {
     res.innerHTML = `<h5>Tabuada do ${numero.value}</h5> `
     for(var cont = 1; cont<=10; cont++) {
         var novatag = document.createElement('div')
+        novatag.setAttribute('class', 'row-calculo')
         var res_calculo = Number(numero.value) * cont
         
         novatag.innerHTML += `${numero.value} X ${cont} = ${res_calculo}`

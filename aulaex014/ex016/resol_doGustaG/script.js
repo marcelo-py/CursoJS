f3454a97190cc56ba955426fda19a1521f629647
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-06 01:51:29 -0300

    contador2.0 versão guanabara

---
+++ aulaex014/ex016/resol_doGustaG/script.js
@@ -0,0 +1,29 @@
+function contar() {
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0) {
+        alert('Erro! faltam dados!')
+    } else {
+        res.innerHTML = 'Contando:' //isso zera pr laço for não recontar novamente
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (p <= 0) {
+            alert('Passo 0, iremos contar de 1 em 1')
+            p = 1
+        }
+        if (i < f) {
+            for (let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            for (let c = i; c >= f; c -= p)
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        res.innerHTML += '\u{1F4A5}'
+    }
+}

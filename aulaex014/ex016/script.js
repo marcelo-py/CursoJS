5e8eadeade5a9ac32c01bbe059b001b4e29af2e3
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-05 22:43:27 -0300

    ex016 finalizado!

--- aulaex014/ex016/script.js
+++ aulaex014/ex016/script.js
@@ -3,10 +3,16 @@ function contar() {
     var final = document.getElementById('atenumero')
     var passo = document.querySelector('input#passo')
     var res = document.querySelector('div#res')
-
-    res.innerHTML = ''
-
+    
     var cont = Number.parseInt(inicial.value)
+    res.innerHTML = ''
+    if (inicial.value.length == 0 || final.value.length == 0) {
+        alert('O valor inicial e nem final não podem ficar vazios')
+    } else if (passo.value.length == 0) {
+        alert('Você não indicou os passou então vamos contar de 1 em 1')
+        passo.value = 1
+    }
+    
     while (cont <= final.value) {
         var novatag = document.createElement('span')
         res.appendChild(novatag)

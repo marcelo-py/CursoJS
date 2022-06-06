941e3e58881538284cf8036b56e6c0f334e8bf56
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-05 23:10:54 -0300

    ex017 finished (segura segura a mais nova fera do JS)

--- aulaex014/ex017/script.js
+++ aulaex014/ex017/script.js
@@ -1,3 +1,14 @@
 function calcular() {
+    var numero = document.getElementById('ntabuada')
+    var res = document.getElementById('res')
     
+    res.innerHTML = ''
+    res.innerHTML = `<h5>Tabuada do ${numero.value}</h5> `
+    for(var cont = 1; cont<=10; cont++) {
+        var novatag = document.createElement('div')
+        var res_calculo = Number(numero.value) * cont
+        
+        novatag.innerHTML += `${numero.value} X ${cont} = ${res_calculo}`
+        res.appendChild(novatag)
+    }
 }

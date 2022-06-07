232caa68cf13ceefff2583876b3a041336b10586
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-07 16:38:10 -0300

    primeira pratica de array

---
+++ aula15/pratica1.js
@@ -0,0 +1,26 @@
+let array = ['Laranja', 'Manga', 'Abacaixi', 'Jaca']
+
+/*console.log(array.length)
+for (let fruta in array) {
+    console.log(array[fruta])
+}
+*/
+
+/*cont = 0
+
+while (cont < array.length) {
+    console.log(array[cont])
+    cont++
+}
+*/
+
+/*cont = 0
+do {
+    console.log(array[cont])
+    cont += 1
+} while (cont < array.length)
+*/
+
+for (let cont=0; cont < array.length; cont+=1) {
+    console.log(array[cont])
+}

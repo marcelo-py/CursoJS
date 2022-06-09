3234e3d8b2f0863fa1f9ededf281ed5d8000ec34
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-09 16:17:05 -0300

    Jogo da velha finalizado. Birl!

--- jogo_da_velha/script.js
+++ jogo_da_velha/script.js
@@ -20,8 +20,71 @@ function caixa() {
         }
         container.innerHTML = marcados
         if (caixas[0].innerHTML == 'X' && caixas[1].innerHTML == 'X' && caixas[2].innerHTML == 'X') {
-            res.innerHTML = 'Primeira linha X ganhou'
+            caixas[0].style.background = 'red'
+            caixas[1].style.background = 'red'
+            caixas[2].style.background = 'red'
+            res.innerHTML = 'X Ganhou na primeira linha'
+        } else if (caixas[3].innerHTML == 'X' && caixas[4].innerHTML == 'X' && caixas[5].innerHTML == 'X') {
+            caixas[3].style.background = 'red'
+            caixas[4].style.background = 'red'
+            caixas[5].style.background = 'red'
+            res.innerHTML = 'X Ganhou na segunda linha'
+        } else if (caixas[6].innerHTML == 'X' && caixas[7].innerHTML == 'X' && caixas[8].innerHTML == 'X'){
+            caixas[6].style.background = 'red'
+            caixas[7].style.background = 'red'
+            caixas[8].style.background = 'red'
+            res.innerHTML = 'X Ganhou na terceira linha'
+        } else if (caixas[0].innerHTML == 'O' && caixas[1].innerHTML == 'O' && caixas[2].innerHTML == 'O') {
+            caixas[0].style.background = 'green'
+            caixas[1].style.background = 'green'
+            caixas[2].style.background = 'green'
+            res.innerHTML = 'O Ganhou na primeira linha'
+        } else if (caixas[3].innerHTML == 'O' && caixas[4].innerHTML == 'O' && caixas[5].innerHTML == 'O') {
+            caixas[3].style.background = 'green'
+            caixas[4].style.background = 'green'
+            caixas[5].style.background = 'green'
+            res.innerHTML = 'O Ganhou na segunda linha'
+        } else if (caixas[6].innerHTML == 'O' && caixas[7].innerHTML == 'O' && caixas[8].innerHTML == 'O') {
+            caixas[6].style.background = 'green'
+            caixas[7].style.background = 'green'
+            caixas[8].style.background = 'green'
+            res.innerHTML = 'O Ganhou na terceira linha'
         }
+        
+        if (caixas[0].innerHTML == 'X' && caixas[3].innerHTML == 'X' && caixas[6].innerHTML == 'X') {
+            caixas[0].style.background = 'red'
+            caixas[3].style.background = 'red'
+            caixas[6].style.background = 'red'
+            res.innerHTML = 'X Ganhou na primeira coluna'
+        } else if (caixas[1].innerHTML == 'X' && caixas[4].innerHTML == 'X' && caixas[7].innerHTML == 'X') {
+            caixas[1].style.background = 'red'
+            caixas[4].style.background = 'red'
+            caixas[7].style.background = 'red'
+            res.innerHTML = 'X Ganhou na segunda coluna'
+        } else if (caixas[2].innerHTML == 'X' && caixas[5].innerHTML == 'X' && caixas[8].innerHTML == 'X') {
+            caixas[2].style.background = 'red'
+            caixas[5].style.background = 'red'
+            caixas[8].style.background = 'red'
+            res.innerHTML = 'X Ganhou na terceira coluna'
+        } else if (caixas[0].innerHTML == 'O' && caixas[3].innerHTML == 'O' && caixas[6].innerHTML == 'O') {
+            caixas[0].style.background = 'green'
+            caixas[3].style.background = 'green'
+            caixas[6].style.background = 'green'
+            res.innerHTML = 'O Ganhou na primeira coluna'
+        } else if (caixas[1].innerHTML == 'O' && caixas[4].innerHTML == 'O' && caixas[7].innerHTML == 'O') {
+            caixas[1].style.background = 'green'
+            caixas[4].style.background = 'green'
+            caixas[7].style.background = 'green'
+            res.innerHTML = 'O Ganhou na segunda coluna'
+        } else if (caixas[2].innerHTML == 'O' && caixas[5].innerHTML == 'O' && caixas[8].innerHTML == 'O') {
+            caixas[2].style.background = 'green'
+            caixas[5].style.background = 'green'
+            caixas[8].style.background = 'green'
+            res.innerHTML = 'O Ganhou na terceira coluna'
+        }
+    }
+    if (marcados.length == 9 && res.innerHTML.length == 0) {
+        res.innerHTML = 'Sem vencedor!'
     }
     
 }

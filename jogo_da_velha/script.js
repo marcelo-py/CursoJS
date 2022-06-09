878df596355b13f598bb07ce6a8523cd9d5836dd
Marcelo Araújo <marcelo.araujo.999s@gmail.com>
2022-06-09 15:23:59 -0300

    logica Linda de se ver, maravilha, coisa formosa

--- jogo_da_velha/script.js
+++ jogo_da_velha/script.js
@@ -1,5 +1,6 @@
 var caixas = document.getElementsByTagName('button')
 var container = document.getElementsByTagName('div')[1]
+var res = document.getElementsByTagName('div')[2]
 
 var marcados = []
 
@@ -9,15 +10,18 @@ for (let c = 0; c<caixas.length; c++) {
 }
 
 function caixa() {
-
     if (this.innerHTML.length == 0) {
         if (marcados.length == 0 || marcados[marcados.length - 1] == 'O') {
-        marcados.push('X')
-        this.innerHTML = 'X'
+            marcados.push('X')
+            this.innerHTML = 'X'
         } else {
             marcados.push('O')
             this.innerHTML = 'O'
         }
         container.innerHTML = marcados
+        if (caixas[0].innerHTML == 'X' && caixas[1].innerHTML == 'X' && caixas[2].innerHTML == 'X') {
+            res.innerHTML = 'Primeira linha X ganhou'
+        }
     }
+    
 }
